20e68bc097e30a4c4dd41294b4babb0a0199697b
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2023-09-21 20:41:40 +0600

    Solve logo and hole url

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand font-weight-bold" href="/vue/ecom">Ecom.</a>
+            <a class="navbar-brand font-weight-bold" href="/">Ecom.</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -13,7 +13,7 @@
                 <ul class="navbar-nav">
 
                     <li class="nav-item active">
-                        <a class="nav-link" href="/vue/ecom">Home</a>
+                        <a class="nav-link" href="/">Home</a>
                     </li>
 
                 </ul>
